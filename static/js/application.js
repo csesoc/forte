ecb62d5ea697ebf6ca7b69145271143ade393ad2
John Wiseheart <johnwiseheart@gmail.com>
2013-12-21 02:18:10 +1100

    Fixed animations and sorting, slightly modified UI to imrpove animation and stuff... yeahh...
    …sexy animations…

--- static/js/application.js
+++ static/js/application.js
@@ -28,8 +28,7 @@ var upvote = function(song_id, playlist_hash, song_votes) {
   } else {
     $j.post("/playlists/" + playlist_hash + "/" + song_id + "/down/" + Math.abs(change_of_votes));
   }
-  votes_id = "#votes_" + song_id;
-  $j(votes_id).text(song_votes);
+  $j("#votes_" + song_id).text(song_votes);
   sortSongs();
   addToCookie(song_id, vote);
 };
@@ -39,6 +38,7 @@ var downvote = function(song_id, playlist_hash, song_votes) {
   up_id = "#up_" + song_id;
   down_id = "#down_" + song_id;
   song_votes = parseInt(song_votes);
+  console.log
   original_votes = song_votes;
   if ($j(down_id).hasClass('secondary') === true) {
     $j(down_id).removeClass('secondary');
@@ -59,47 +59,42 @@ var downvote = function(song_id, playlist_hash, song_votes) {
   } else {
     $j.post("/playlists/" + playlist_hash + "/" + song_id + "/down/" + Math.abs(change_of_votes));
   }
-  votes_id = "#votes_" + song_id;
-  $j(votes_id).text(song_votes);
+  $j("#votes_" + song_id).text(song_votes);
   sortSongs();
   addToCookie(song_id, vote);
 };
 
+
+
 var sortSongs = function(){
-  divList = $j('#sortable > div').each(function(a, b) { 
-    return parseInt($j("p", a).text())
-  });
+ // $j('#Grid>li').tsort('div>div>p',{order:'desc'});
 
-  function getVotes(a) {
-    return parseInt($j("p", a).text());
-  }
+  var $Ul = $j('#Grid');
+$Ul.css({position:'relative',height:$Ul.height(),display:'block'});
+var iLnH;
+var $Li = $j('ul>li');
+$Li.each(function(i,el){
+  var iY = $j(el).position().top;
+  $j.data(el,'h',iY);
+  if (i===1) iLnH = iY;
+});
+$Li.tsort('div>div>h5',{order:'desc'}).each(function(i,el){
+  var $El = $j(el);
+  var iFr = $j.data(el,'h');
+  var iTo = i*iLnH;
+  $El.css({position:'absolute',top:iFr}).animate({top:iTo},500);
+});
 
-  var votes = new Array();
-  divList.each(function(i, a) {
-    votes[i] = getVotes(a);
-  });
-
-  divList.each(function(i, a) {
-    if (!isNaN(votes[i-1])) {
-      if (votes[i-1] < votes[i]) {
-        var toAppend = divList[i-1];
-        $j(toAppend).slideUp().delay(300);
-        setTimeout(function() {
-          $j(a).after(toAppend);
-        }, 350);
-        $j(toAppend).slideDown();
-      }
-    }
-  });
+ //$j('#Grid').mixitup('sort',['votes','asc']);
 };
 
 var checkCookie = function() {
   var forte = jar.get('forte');
   if (forte != null) {
-    console.log(forte);
+   // console.log(forte);
   } else {
     forte = {};
-    jar.put('forte', forte);    
+    jar.put('forte', forte);  
   }
   return;
 };
